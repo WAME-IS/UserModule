413a9f7384bed4c484a6d1a6684dcb7527636fc1
MacMini2 <djsucho@gmail.com>
2016-04-15 09:24:14 +0200

    UserModule

--- repositories/UserInfoRepository.php
+++ repositories/UserInfoRepository.php
@@ -4,11 +4,5 @@ namespace Wame\UserModule\Repositories;
 
 class UserInfoRepository extends \Wame\Core\Repositories\BaseRepository
 {
-	const TABLE_NAME = 'user_info';
-	
-	public function __construct(\Nette\DI\Container $container) 
-	{
-		parent::__construct($container, self::TABLE_NAME);
-	}
 	
 }
