83d4b0e34f6ee9ae05bc6831be7819c05505c968
MacMini2 <djsucho@gmail.com>
2016-04-22 08:59:04 +0200

    add user FormContainers

---
+++ forms/passwordRepeat/PasswordRepeatFormContainer.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Wame\UserModule\Forms;
+
+use Nette\Application\UI\Form;
+use Wame\DynamicObject\Forms\BaseFormContainer;
+
+class PasswordRepeatFormContainer extends BaseFormContainer
+{
+    public function render() 
+	{
+        $this->template->_form = $this->getForm();
+        $this->template->render(__DIR__ . '/default.latte');
+    }
+
+    public function configure() 
+	{
+		$form = $this->getForm();
+		
+		$form->addPassword('password_repeat', _('Password repeat'))
+				->setType('password')
+				->addRule(Form::FILLED, _('Password can not be empty'))
+				->addRule(Form::EQUAL, _('Passwords must be the same'), $form['password']);
+    }
+	
+}
