82d92bfa44e168120e3af8cf06565538ac7144de
MacMini2 <djsucho@gmail.com>
2016-06-02 11:38:21 +0200

    ADD methods in UserInCompanyRepository

--- repositories/UserInCompanyRepository.php
+++ repositories/UserInCompanyRepository.php
@@ -7,6 +7,16 @@ use Wame\UserModule\Entities\UserInCompanyEntity;
 
 class UserInCompanyRepository extends BaseRepository
 {
+	public function __construct(
+		\Nette\DI\Container $container, 
+		\Kdyby\Doctrine\EntityManager $entityManager, 
+		\h4kuna\Gettext\GettextSetup $translator, 
+		\Nette\Security\User $user
+	) {
+		parent::__construct($container, $entityManager, $translator, $user, UserInCompanyEntity::class);
+	}
+	
+	
 	/**
 	 * Add user to company
 	 * 
@@ -18,6 +28,55 @@ class UserInCompanyRepository extends BaseRepository
 		$this->entityManager->persist($userInCompanyEntity);
 		
 		return $userInCompanyEntity;
+	}
+	
+	
+	/**
+	 * Get company list
+	 * 
+	 * @param int $userId
+	 * @return array
+	 * @throws \Exception
+	 */
+	public function getCompanyList($userId = null)
+	{
+		if (!$userId) {
+			if (!$this->user->isLoggedIn()) {
+				throw new \Exception(_('To start this action must be signed.'));
+			}
+			
+			$userId = $this->user->id;
+		}
+		
+		$find = $this->find(['user.id' => $userId, 'company.status' => CompanyRepository::STATUS_ACTIVE]);
+
+		$return = [];
+		
+		foreach ($find as $row) {
+			$return[$row->company->getId()] = $row->company;
+		}
+
+		return $return;
+	}
+	
+	
+	/**
+	 * Get company pairs
+	 * 
+	 * @param int $userId
+	 * @return array
+	 */
+	public function getCompanyPairs($userId = null, $value = 'name', $key = 'id')
+	{
+		$return = [];
+		
+		$companies = $this->getCompanyList($userId);
+		
+		foreach ($companies as $company) {
+			$return[$company->$key] = $company->$value;
+		}
+		
+		return $return;
 	}
 
 }
