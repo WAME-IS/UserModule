7a8147e7f1aa6358c85c77fcc0d1ea49065dfda9
Stanislav Červeňák <stano@wame.sk>
2016-07-18 16:35:49 +0200

    FIX User registration

--- forms/user/birthdate/BirthdateFormContainer.php
+++ forms/user/birthdate/BirthdateFormContainer.php
@@ -2,7 +2,9 @@
 
 namespace Wame\UserModule\Forms;
 
+use Wame\Utils\Date;
 use Wame\DynamicObject\Forms\BaseFormContainer;
+use Wame\UserModule\Entities\UserInfoEntity;
 
 
 interface IBirthdateFormContainerFactory
@@ -29,8 +31,33 @@ class BirthdateFormContainer extends BaseFormContainer
 		$form = $this->getForm();
 
 		if ($object->userEntity->info->birthdate) {
-			$form['birthdate']->setDefaultValue(\Wame\Utils\Date::toDateTime($object->userEntity->info->birthdate, 'd.m.Y'));
+			$form['birthdate']->setDefaultValue(Date::toDateTime($object->userEntity->info->birthdate, 'd.m.Y'));
 		}
 	}
+
+    
+    /**
+     * Create
+     * 
+     * @param \Nette\Application\UI\Form $form
+     * @param array $values
+     * @param \Nette\Application\UI\Presenter $presenter
+     */
+    public function create($form, $values, $presenter)
+    {
+        $userInfoEntity = $presenter->getStatus()->get('userInfoEntity');
+
+        if (!$userInfoEntity) {
+            $userInfoEntity = new UserInfoEntity();
+        }
+        
+        if ($values['birthdate']) {
+			$userInfoEntity->setBirthdate(Date::toDateTime($values['birthdate'], 'Y-m-d'));
+		} else {
+			$userInfoEntity->setBirthdate(null);
+		}
+
+        $presenter->getStatus()->set('userInfoEntity', $userInfoEntity);
+    }
 
 }
--- forms/user/degree/DegreeFormContainer.php
+++ forms/user/degree/DegreeFormContainer.php
@@ -3,6 +3,7 @@
 namespace Wame\UserModule\Forms;
 
 use Wame\DynamicObject\Forms\BaseFormContainer;
+use Wame\UserModule\Entities\UserInfoEntity;
 
 
 interface IDegreeFormContainerFactory
@@ -30,5 +31,26 @@ class DegreeFormContainer extends BaseFormContainer
 
 		$form['degree']->setDefaultValue($object->userEntity->info->degree);
 	}
+
+    
+    /**
+     * Create
+     * 
+     * @param \Nette\Application\UI\Form $form
+     * @param array $values
+     * @param \Nette\Application\UI\Presenter $presenter
+     */
+    public function create($form, $values, $presenter)
+    {
+        $userInfoEntity = $presenter->getStatus()->get('userInfoEntity');
+
+        if (!$userInfoEntity) {
+            $userInfoEntity = new UserInfoEntity();
+        }
+
+        $userInfoEntity->setDegree($values['degree']);
+
+        $presenter->getStatus()->set('userInfoEntity', $userInfoEntity);
+    }
 
 }
