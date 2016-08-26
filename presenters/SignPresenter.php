d9edf6d63868b65ad931cb9a5ea06a0986146a72
shadd0w <rene.gmitter@wame.sk>
2016-08-26 12:02:01 +0200

    UPDATE User

--- presenters/SignPresenter.php
+++ presenters/SignPresenter.php
@@ -4,6 +4,7 @@ namespace App\UserModule\Presenters;
 
 use Wame\UserModule\Forms\SignInForm;
 use Wame\UserModule\Forms\SignUpForm;
+use Wame\UserModule\Repositories\UserRepository;
 
 class SignPresenter extends \App\Core\Presenters\BasePresenter
 {	
@@ -12,6 +13,9 @@ class SignPresenter extends \App\Core\Presenters\BasePresenter
 
 	/** @var SignUpForm @inject */
 	public $signUpForm;
+    
+    /** @var UserRepository @inject */
+    public $userRepository;
 	
 	
 	public function actionIn()
@@ -41,6 +45,28 @@ class SignPresenter extends \App\Core\Presenters\BasePresenter
     
     public function actionVerify()
     {
+        $error = false;
+        $email = $this->getParameter('email');
+        $hash = $this->getParameter('hash');
+        
+        if($email & $hash) {
+            $user = $this->userRepository->get(['token' => $hash, 'email' => $email, 'status' => UserRepository::STATUS_VERIFY_EMAIL]);
+            
+            if($user) {
+                $user->status = UserRepository::STATUS_ACTIVE;
+            } else {
+                $error = true;
+            }
+        } else {
+            $error = true;
+        }
+        
+        if(!$error) {
+            $this->flashMessage(_('Your account has been activated.'), 'info');
+        } else {
+            $this->flashMessage(_('Invalid approach, please use the link that has been send to your email.'), 'danger');
+        }
+        
         $this->redirect(':User:Profile:');
     }
 	
