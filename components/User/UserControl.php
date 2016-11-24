e087e1911ebef07f560e5c6550b4a6ce1463c5cb
shadd0w <rene.gmitter@wame.sk>
2016-11-24 11:56:45 +0100

    FIX UserControl

--- components/User/UserControl.php
+++ components/User/UserControl.php
@@ -2,10 +2,10 @@
 
 namespace Wame\UserModule\Components;
 
+use Nette\Security\User;
 use Wame\UserModule\Entities\UserEntity;
 use Wame\ChameleonComponents\Components\SingleEntityControl;
 use Wame\ListControl\Components\IEntityControlFactory;
-
 
 interface IUserControlFactory extends IEntityControlFactory
 {
@@ -13,23 +13,29 @@ interface IUserControlFactory extends IEntityControlFactory
     public function create($entity = null);
 }
 
-
 class UserControl extends SingleEntityControl
 {
-    public function __construct(\Nette\DI\Container $container, $entity = null) 
+    public function __construct(\Nette\DI\Container $container, User $user, $entity = null) 
     {
+        // if not provided and logged, set entity to logged user
+        if(!$entity && $user) {
+            $entity = $user->getEntity();
+        }
+        
         parent::__construct($container, $entity);
-        
-        
     }
     
+    
+    /** {@inheritDoc} */
     protected function getEntityType()
     {
         return UserEntity::class;
     }
     
     
-    public function render() {
+    /** {@inheritDoc} */
+    public function render()
+    {
         parent::render();
         
         $this->template->columns = ['column1', '2'];
