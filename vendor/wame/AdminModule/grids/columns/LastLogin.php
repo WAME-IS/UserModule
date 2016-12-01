a72d4aa06ca59975527b499bae2871ab48903fee
Stanislav Červeňák <stano@wame.sk>
2016-12-01 09:39:05 +0100

    EDIT DataGrid date columns

--- vendor/wame/AdminModule/grids/columns/LastLogin.php
+++ vendor/wame/AdminModule/grids/columns/LastLogin.php
@@ -2,7 +2,10 @@
 
 namespace Wame\UserModule\Vendor\Wame\AdminModule\Grids\Columns;
 
+use Nette\Utils\Html;
 use Wame\DataGridControl\BaseGridItem;
+use Wame\Utils\Date;
+
 
 class LastLogin extends BaseGridItem
 {
@@ -10,11 +13,17 @@ class LastLogin extends BaseGridItem
 	public function render($grid)
 	{
 		$grid->addColumnDateTime('lastLogin', _('Last login'))
-                ->setFormat('d.m.Y - H:i:s')
+                ->setFormat('d.m.Y H:i')
+                ->setRenderer(function($user) {
+                    $date = $user->getLastLogin();
+
+                    return Html::el('div')->setText(Date::toString($date, 'd.m.Y')) . Html::el('small')->setText(Date::toString($date, 'H:i:s'));
+                })
+                ->setTemplateEscaping(false)
 				->setSortable()
 				->setFilterDate();
-		
+
 		return $grid;
 	}
-    
+
 }
--- vendor/wame/AdminModule/grids/columns/RegisterDate.php
+++ vendor/wame/AdminModule/grids/columns/RegisterDate.php
@@ -2,7 +2,10 @@
 
 namespace Wame\UserModule\Vendor\Wame\AdminModule\Grids\Columns;
 
+use Nette\Utils\Html;
 use Wame\DataGridControl\BaseGridItem;
+use Wame\Utils\Date;
+
 
 class RegisterDate extends BaseGridItem
 {
@@ -10,11 +13,17 @@ class RegisterDate extends BaseGridItem
 	public function render($grid)
 	{
 		$grid->addColumnDateTime('registerDate', _('Register date'))
-                ->setFormat('d.m.Y - H:i:s')
+                ->setFormat('d.m.Y H:i')
+                ->setRenderer(function($user) {
+                    $date = $user->getRegisterDate();
+
+                    return Html::el('div')->setText(Date::toString($date, 'd.m.Y')) . Html::el('small')->setText(Date::toString($date, 'H:i:s'));
+                })
+                ->setTemplateEscaping(false)
 				->setSortable()
 				->setFilterDate();
-		
+
 		return $grid;
 	}
-    
+
 }
