1cb9635f7990ffc34133fccb6c575adcbce13076
Stanislav Červeňák <stano@wame.sk>
2016-12-28 17:22:02 +0100

    FIX User extension getEntity()

--- models/UserEntityExtensionMethod.php
+++ models/UserEntityExtensionMethod.php
@@ -4,12 +4,16 @@ namespace Wame\UserModule\Models;
 
 use Nette\Security\User;
 use Wame\UserModule\Repositories\UserRepository;
+use Nette\Utils\ObjectMixin;
 
 class UserEntityExtensionMethod
 {
 
     public function __construct(UserRepository $userRepository)
     {
+        ObjectMixin::setExtensionMethod(User::class, 'getEntity', function(User $user) use ($userRepository) {
+            return $userRepository->getUserById($user->getId());
+        });
 //        User::extensionMethod('getEntity', function(User $user) use ($userRepository) {
 //            return $userRepository->getUserById($user->getId());
 //        });
