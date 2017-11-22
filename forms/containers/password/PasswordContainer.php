8016989515f2f1c5170f8def64e29357c888e6b7
Stanislav Červeňák <stano@wame.sk>
2017-11-22 09:39:26 +0100

    UPDATE password input - add rules

--- forms/containers/password/PasswordContainer.php
+++ forms/containers/password/PasswordContainer.php
@@ -2,6 +2,7 @@
 
 namespace Wame\UserModule\Forms\Containers;
 
+use Nette\Application\UI\Form;
 use Nette\Security\Passwords;
 use Wame\DynamicObject\Forms\Containers\BaseContainer;
 use Wame\DynamicObject\Registers\Types\IBaseContainer;
@@ -19,9 +20,16 @@ class PasswordContainer extends BaseContainer
     /** {@inheritDoc} */
     public function configure()
 	{
+	    $minLength = 8;
+	    $condition = _('Password must contain at least one number, uppercase and lowercase letter');
+
 		$this->addPassword('password', _('Password'))
-            ->setType('password')
-            ->setRequired(true);
+                ->setOption('description', sprintf(_('Password must be at least %s characters long and must contain at least one number, uppercase and lowercase letter.'), $minLength))
+                ->setRequired(true)
+                ->addRule(Form::MIN_LENGTH, _('Password must be at least %d characters long'), $minLength)
+                ->addRule(Form::PATTERN, $condition, '.*[0-9].*')
+                ->addRule(Form::PATTERN, $condition, '.*[A-Z].*')
+                ->addRule(Form::PATTERN, $condition, '.*[a-z].*');
     }
 
 
