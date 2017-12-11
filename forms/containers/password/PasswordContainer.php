c0145027a76dcaed5bd9d49aa82372f1f12b8156
Stanislav Červeňák <stano@wame.sk>
2017-12-11 13:00:30 +0100

    CREATE password validate form container

--- forms/containers/password/PasswordContainer.php
+++ forms/containers/password/PasswordContainer.php
@@ -20,16 +20,8 @@ class PasswordContainer extends BaseContainer
     /** {@inheritDoc} */
     public function configure()
 	{
-	    $minLength = 8;
-	    $condition = _('Password must contain at least one number, uppercase and lowercase letter');
-
 		$this->addPassword('password', _('Password'))
-                ->setOption('description', sprintf(_('Password must be at least %s characters long and must contain at least one number, uppercase and lowercase letter.'), $minLength))
-                ->setRequired(true)
-                ->addRule(Form::MIN_LENGTH, _('Password must be at least %d characters long'), $minLength)
-                ->addRule(Form::PATTERN, $condition, '.*[0-9].*')
-                ->addRule(Form::PATTERN, $condition, '.*[A-Z].*')
-                ->addRule(Form::PATTERN, $condition, '.*[a-z].*');
+                ->setRequired(true);
     }
 
 
---
+++ forms/containers/passwordValidate/PasswordValidateContainer.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Wame\UserModule\Forms\Containers;
+
+use Nette\Application\UI\Form;
+use Nette\Security\Passwords;
+use Wame\DynamicObject\Forms\Containers\BaseContainer;
+use Wame\DynamicObject\Registers\Types\IBaseContainer;
+
+
+interface IPasswordValidateContainerFactory extends IBaseContainer
+{
+	/** @return PasswordValidateContainer */
+	public function create();
+}
+
+
+class PasswordValidateContainer extends BaseContainer
+{
+    /** {@inheritDoc} */
+    public function configure()
+	{
+	    $minLength = 8;
+	    $condition = _('Password must contain at least one number, uppercase and lowercase letter');
+
+		$this->addPassword('password', _('Password'))
+                ->setOption('description', sprintf(_('Password must be at least %s characters long and must contain at least one number, uppercase and lowercase letter.'), $minLength))
+                ->setRequired(true)
+                ->addRule(Form::MIN_LENGTH, _('Password must be at least %d characters long'), $minLength)
+                ->addRule(Form::PATTERN, $condition, '.*[0-9].*')
+                ->addRule(Form::PATTERN, $condition, '.*[A-Z].*')
+                ->addRule(Form::PATTERN, $condition, '.*[a-z].*');
+    }
+
+
+    /** {@inheritDoc} */
+    public function create($form, $values)
+    {
+        $form->getEntity()->setPassword($values['password']);
+    }
+
+
+    /** {@inheritDoc} */
+    public function update($form, $values)
+    {
+        $form->getEntity()->setPassword($values['password']);
+    }
+
+}
